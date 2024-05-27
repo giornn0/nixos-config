f87f4a5b5e00e6ddd929207dc5f2a905a6c51161
giornn0 <rusty.arrebol@proton.me>
2024-05-27 09:11:43 -0300

    feat: hyprlock working

--- home/hyprland/hyprlock.nix
+++ home/hyprland/hyprlock.nix
@@ -2,11 +2,11 @@
   font_family = "Noto";
 in {
   programs.hyprlock.enable = true;
-  settings = {
+  programs.hyprlock.settings = {
     general = {
       disable_loading_bar = true;
       hide_cursor = false;
-      no_fade_in = true;
+      no_fade_in = false;
     };
 
     background = [
@@ -18,34 +18,58 @@ in {
 
     input-field = [
       {
-        monitor = "eDP-1";
-
-        size = "300, 50";
-
-        outline_thickness = 1;
-
-        # outer_color = "rgb(${c.primary})";
-        # inner_color = "rgb(${c.on_primary_container})";
-        # font_color = "rgb(${c.primary_container})";
-
-        fade_on_empty = false;
-        placeholder_text = ''<span font_family="${font_family}" foreground="##${""}">Password...</span>'';
-
-        dots_spacing = 0.2;
-        dots_center = true;
+        monitor = {
+          size = "200, 50";
+          outline_thickness = 3;
+          dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
+          dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+          dots_center = true;
+          dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
+          outer_color = "rgb(151515)";
+          inner_color = "rgb(FFFFFF)";
+          font_color = "rgb(10, 10, 10)";
+          fade_on_empty = true;
+          fade_timeout = 1000; # Milliseconds before fade_on_empty is triggered.
+          placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
+          hide_input = false;
+          rounding = -1; # -1 means complete rounding (circle/oval)
+          check_color = "rgb(204, 136, 34)";
+          fail_color = "rgb(204, 34, 34)"; # if authentication failed, changes outer_color and fail message color
+          fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>"; # can be set to empty
+          fail_transition = 300; # transition time in ms between normal outer_color and fail_color
+          capslock_color = -1;
+          numlock_color = -1;
+          bothlock_color = -1; # when both locks are active. -1 means don't change outer color (same for above)
+          invert_numlock = false; # change color if numlock is off
+          swap_font_color = false; # see below
+          position = "0, -20";
+          halign = "center";
+          valign = "center";
+        };
       }
     ];
-
     label = [
       {
         monitor = "";
         text = "$TIME";
-        inherit font_family;
-        font_size = 50;
-        # color = "rgb(${c.primary})";
-
+        color = "rgba(200, 200, 200, 1.0)";
+        font_size = "55";
+        font_family = "${font_family}";
+        shadow_passes = 5;
+        shadow_size = 10;
         position = "0, 80";
-
+        valign = "center";
+        halign = "center";
+      }
+      {
+        monitor = "";
+        text = "$USER";
+        color = "rgba(200, 200, 200, 1.0)";
+        font_size = 20;
+        font_family = "${font_family}";
+        shadow_passes = 5;
+        shadow_size = 10;
+        position = "0, 40";
         valign = "center";
         halign = "center";
       }
