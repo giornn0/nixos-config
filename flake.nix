93a18822362b823e70f3fc9ba40ac1d5cb101179
giornn0 <rusty.arrebol@proton.me>
2024-11-18 22:48:54 -0300

    chore: dep update

--- flake.nix
+++ flake.nix
@@ -1,49 +1,19 @@
 {
   description = "Giornn0's Flake Configuration";
-
-  # This is the standard format for flake.nix.
-  # `inputs` are the dependencies of the flake,
-  # and `outputs` function will return all the build results of the flake.
-  # Each item in `inputs` will be passed as a parameter to
-  # the `outputs` function after being pulled and built.
   inputs = {
-    # There are many ways to reference flake inputs.
-    # The most widely used is `github:owner/name/reference`,
-    # which represents the GitHub repository URL + branch/commit-id/tag.
-    # Official NixOS package source, using nixos-23.11 branch here
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    # hyprlock = {
-    #   url = "github:hyprwm/hyprlock";
-    #   inputs.hyprlang.follows = "hyprland/hyprlang";
-    #   inputs.nixpkgs.follows = "hyprland/nixpkgs";
-    #   inputs.systems.follows = "hyprland/systems";
-    # };
-    # home-manager, used for managing user configuration
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    # neovim.url = "github:neovim/neovim?dir=contrib";
-    #helix.url = "github:helix-editor/helix/master";
+    # neovim-flake.url = "github:neovim/neovim?dir=contrib";
+    neovim-nightly-overlay=  {
+url = "github:nix-community/neovim-nightly-overlay";
+     # inputs.nixpkgs.follows = "nixpkgs";
+# inputs.neovim-flake.url = "github:neovim/neovim?dir=contrib&rev=8b98642002d0506d20628683958cb5c97a0dad80"; 
+};
   };
-
-  # `outputs` are all the build result of the flake.
-  #
-  # A flake can have many use cases and different types of outputs.
-  #
-  # parameters in function `outputs` are defined in `inputs` and
-  # can be referenced by their names. However, `self` is an exception,
-  # this special parameter points to the `outputs` itself(self-reference)
-  #
-  # The `@` syntax here is used to alias the attribute set of the
-  # inputs's parameter, making it convenient to use inside the function.
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
