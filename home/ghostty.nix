e525397c780d6153d1ef637eccb6fb64155ab1b6
giornn0 <rusty.arrebol@proton.me>
2025-01-12 17:13:23 -0300

    feat: ghostty working config

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -1,16 +1,19 @@
 { pkgs, ... }: {
   # ......
-  home.packages = with pkgs; [ ghostty ];
 
-  # home.file.".config/ghostty/config".text = ''
-  #   foreground = f8f8f2
-  #   background = 000000
-  #   font_family = "FiraCode Nerd Font"
-  #   cursor-style = "block_hollow"
-  #   cursor-style-blink = true
-  #   term = "256color"
-  #   background-opacity = 0.8
-  #   fullscreen = true
-  #   cursor-color = 8fee96
-  # '';
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      foreground = "f8f8f2";
+      background = "000000";
+      font-family = "FiraCode Nerd Font";
+      font-size = 16;
+      cursor-style-blink = true;
+      term = "256color";
+      background-opacity = 0.8;
+      fullscreen = true;
+      cursor-color = "8fee96";
+
+    };
+  };
 }
