ee8ea66530fd74a15e81070ecfe9b79e350d96ba
giornn0 <rusty.arrebol@proton.me>
2025-08-06 16:31:06 -0300

    feat: small tweaks to wleave

--- home/themes/wleave/default.nix
+++ home/themes/wleave/default.nix
@@ -41,46 +41,52 @@ in {
   '';
 
   home.file.".config/wleave/style.css".text = ''
+    * {
+      background-image: none;
+    }
+
     window {
-    	background-color: rgba(12, 12, 12, 0.9);
+      background-color: rgba(12, 12, 12, 0.9);
     }
 
     button {
-      color: rgba(255,255,255, 1);
-    	background-color: rgba(30, 30, 30, 0.95);
-    	border-style: solid;
-    	border-width: 2px;
-    	background-repeat: no-repeat;
-    	background-position: center;
+      color: #ffffff;
+      background-color: #1e1e1e;
+      border-style: solid;
+      border-width: 2px;
+      background-repeat: no-repeat;
+      background-position: center;
     }
 
-    button:focus, button:active, button:hover {
-    	background-color: rgba(55, 0, 179, 1);
-    	outline-style: none;
+    button:focus,
+    button:active,
+    button:hover {
+      background-color: #3700b3;
+      outline-style: none;
     }
 
     #lock {
-        background-image: image(url("${home_directory}/.nixos_config/icons/lock.svg"), url("${home_directory}/.nixos_config/icons/lock.svg"));
+      background-image: url("${home_directory}/.nixos_config/icons/lock.svg");
     }
 
     #logout {
-        background-image: image(url("${home_directory}/.nixos_config/icons/logout.svg"), url("${home_directory}/.nixos_config/icons/logout.svg"));
+      background-image: url("${home_directory}/.nixos_config"/icons/logout.svg");
     }
 
     #suspend {
-        background-image: image(url("${home_directory}/.nixos_config/icons/suspend.svg"), url("${home_directory}/.nixos_config/icons/suspend.svg"));
+      background-image: url("${home_directory}/.nixos_config/icons/suspend.svg");
     }
 
     #hibernate {
-        background-image: image(url("${home_directory}/.nixos_config/icons/hibernate.svg"), url("${home_directory}/.nixos_config/icons/hibernate.svg"));
+      background-image: url("${home_directory}/.nixos_config/icons/hibernate.svg");
     }
 
     #shutdown {
-        background-image: image(url("${home_directory}/.nixos_config/icons/shutdown.svg"), url("${home_directory}/.nixos_config/icons/shutdown.svg"));
+      background-image: url("${home_directory}/.nixos_config/icons/shutdown.svg");
     }
 
     #reboot {
-        background-image: image(url("${home_directory}/.nixos_config/icons/reboot.svg"), url("${home_directory}/.nixos_config/icons/reboot.svg"));
+      background-image: url("${home_directory}/.nixos_config/icons/reboot.svg");
     }
   '';
 }
